78f58d5da489012f6388066a0623eb368bd2b184
Darling <99622500+101Darling@users.noreply.github.com>
2023-06-01 03:59:15 -0400

    Update traffic light - 3 turtles - state machines practice.py
    UPDATE:
    For this iteration, there will be three separate turtles given G, Y, R colors which will be visible or invisible to the user depending on the handler function

--- traffic light - state machines practice.py
+++ traffic light - state machines practice.py
@@ -6,6 +6,10 @@ Purpose:
     This program focuses on state machines.
     Utilizing the process of traffic lights for design implementation.
     The traffic light flashes will have several iterations for further practice with state machines.
+
+    UPDATE:
+    For this iteration there will be three seperate turtles given G,Y,R colors which will be..
+    visible or invisible to the user depending on the handler function
 '''
 
 import turtle # Tess becomes a traffic light.
@@ -32,46 +36,84 @@ def draw_housing():
 
 draw_housing()
 
+tess.hideturtle()
 tess.penup()
 # Position tess onto the place where the green light should be
 tess.forward(40)
 tess.left(90)
 tess.forward(50)
+# ------ print(tess.pos())
 # Turn tess into a big green circle
 tess.shape("circle")
 tess.shapesize(3)
 tess.fillcolor("green")
 
+
+#create a second turtle for the yellow light
+alex_Y = turtle.Turtle(shape="circle")
+alex_Y.hideturtle()
+#alex_Y.hideturtle()
+alex_Y.shapesize(3)
+alex_Y.penup()
+alex_Y.fillcolor("orange")
+alex_Y.goto(40.00,120.00)
+
+
+#create a third turtle for the red light
+alex_R = turtle.Turtle(shape="circle")
+alex_R.hideturtle()
+#alex_Y.hideturtle()
+alex_R.shapesize(3)
+alex_R.penup()
+alex_R.fillcolor("red")
+alex_R.goto(40.00,190.00)
+
 # A traffic light is a kind of state machine with three states,
 # Green, Orange, Red. We number these states 0, 1, 2
-# When the machine changes state, we change tess’ position and
-# her fillcolor.
+# When the machine changes state, we change the turtle visiblitiy
+#...given the 3 different turtle colors
 
 # This variable holds the current state of the machine
 state_num = 0
 
-def advance_state_machine():
-    global state_num 
-    if state_num == 0: # Transition from state 0 to state 1
-        # Bind the event handler to a timer
-        wn.ontimer(advance_state_machine,1000)
-        tess.forward(70)
-        tess.fillcolor("orange")
-        state_num = 1
-    elif state_num == 1: # Transition from state 1 to state 2
-        tess.forward(70)
-        tess.fillcolor("red")
-        # Bind the event handler to a timer
-        wn.ontimer(advance_state_machine,1000)
-        state_num = 2
-    else: # Transition from state 2 to state 0
-        tess.back(140)
-        tess.fillcolor("green")
-        state_num = 0
-        # Bind the event handler to a timer
-        wn.ontimer(advance_state_machine,1000)
+# LIGHT LOCATIONS using .pos() method
+'''
+Yellow(40.00,120.00)
+Red(40.00,190.00)
+Green (40.00,50.00)
+'''
 
-# Bind the event handler to a timer
+counter = 0
+def advance_state_machine():
+    global state_num
+    global counter
+    if counter < 3: #loop traffic program for 3 iterations
+        if state_num == 0: # Transition from state 0 to state 1 while one light remains visible on a timer
+            tess.showturtle()
+            alex_Y.hideturtle()
+            alex_R.hideturtle()
+            state_num = 1
+            wn.ontimer(advance_state_machine,600)
+        elif state_num == 1: # Transition from state 1 to state 2 while one light remains visible on a timer
+            alex_Y.showturtle()
+            alex_R.hideturtle()
+            tess.hideturtle()
+            state_num = 2
+            wn.ontimer(advance_state_machine,600)
+        else: # Transition from state 2 to state 0 while one light remains visible on a timer
+            alex_R.showturtle()
+            alex_Y.hideturtle()
+            tess.hideturtle()
+            state_num = 0
+            wn.ontimer(advance_state_machine,600)
+            counter += 1 #change global counter variable by adding +1
+    else:
+        wn.bye() #Shut the graphics window down
+    
+# Bind the event handler to the space key
+#wn.onkey(advance_state_machine,"space")
+
+#call the function
 advance_state_machine()
 
 wn.listen() # Listen for events
